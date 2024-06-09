ace9f369b2ffa6945dbef66c5081785eb51eff1c
Trinh-Tan-Dat <21520714@gm.uit.edu.vn>
2024-06-09 12:22:23 +0700

    refactor: Improve comment section functionality and UI

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -10,7 +10,7 @@ const Comment = () => {
     const userId = localStorage.getItem('userToken');
     const firstName = localStorage.getItem('firstName');
     const lastName = localStorage.getItem('lastName');
-    const userName = firstName + " " + lastName;
+    const userName = `${firstName} ${lastName}`;
     const [searchParams] = useSearchParams();
     const videoId = searchParams.get('videoId');
 
@@ -21,9 +21,10 @@ const Comment = () => {
     const fetchComments = async () => {
         try {
             const response = await axios.get(`http://localhost:8080/comment/getALLCommentByVideoId/${videoId}`);
-            setComments(response.data);
+            setComments(response.data || []); // Ensure comments is an array
         } catch (error) {
             console.error("Error fetching comments:", error);
+            setComments([]); // Fallback to an empty array in case of error
         }
     };
 
@@ -48,7 +49,7 @@ const Comment = () => {
         <div className="w-full relative">
             <div className="mx-2 absolute top-0 left-0 w-full flex items-center gap-3 mb-6">
                 <input
-                    type="textbox"
+                    type="text"
                     value={commentText}
                     onChange={handleCommentChange}
                     className="px-4 rounded-[10px] py-3 border-[1px] w-[100%]"
@@ -60,9 +61,13 @@ const Comment = () => {
                 />
             </div>
             <div className="h-[80px]"></div>
-            {comments.map((comment, index) => (
-                <MonoComment key={index} comment={comment} fetchComments={fetchComments} />
-            ))}
+            {comments.length > 0 ? (
+                comments.map((comment, index) => (
+                    <MonoComment key={index} comment={comment} fetchComments={fetchComments} />
+                ))
+            ) : (
+                <p className="text-center mt-6">No comments yet. Be the first to comment!</p>
+            )}
         </div>
     );
 }
